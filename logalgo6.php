bc423ef28608dc06066fe9119159c52bf5532937
Pavla Savincova <pavla.savincova@gmail.com>
2025-10-27 08:17:39 +0100

    sunday

--- logalgo6.php
+++ logalgo6.php
@@ -11,9 +11,40 @@ function findLogestWord($words) {
 $words=["jablko", "banán", "třešeň"];
 findLogestWord($words);
 $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function sumEvenNumbers($numbers) {
+function sumEvenNumbers($numbers) 
+{
    $choose=array_filter($numbers, function($num) {
         return $num % 2 == 0;
     });
-    $sum = array_sum($numbers);
+    $sum = array_sum($choose);
     echo "Součet sudých čísel je: " . $sum;
+}
+$words = "Byla velka bourka.";
+    function reverseWords($words) {
+        $words = explode(" ", $words);
+        $reverse = array_reverse($words);
+        $sentence = implode(" ", $reverse);
+        echo "Obrácená věta: " . $sentence;
+    }
+    reverseWords($words);
+
+$array = [1, 2, 3, 4, 5, 3, 6, 2];
+function hasDuplicates($array)
+ {
+    $unique = array_unique($array);
+    if (count($unique) < count($array)) {
+        echo "Pole obsahuje duplicitní hodnoty.";
+    } else {
+        echo "Pole neobsahuje duplicitní hodnoty.";
+    }
+ }
+  hasDuplicates($array);
+
+  function uniqueLetters($string) 
+  {
+    $letters = str_split($string);
+    $uniqueLetters = array_unique($letters);
+    echo "Unikátní písmena ve slově: " . implode(", ", $uniqueLetters);
+}
+$string = "programování";
+uniqueLetters($string);
